f3e1bbd792e91e32415048571577efa52832fb35
adammck <adam.mckaig@gmail.com>
2010-03-13 14:18:28 -0500

    made example model fields nullable.

--- person/models.py
+++ person/models.py
@@ -7,5 +7,5 @@ from django.db import models
 
 class Person(models.Model):
     name   = models.CharField("First Name", max_length=30)
-    height = models.IntegerField("Height (cm)")
-    weight = models.IntegerField("Weight (kg)")
+    height = models.IntegerField("Height (cm)", null=True, blank=True)
+    weight = models.IntegerField("Weight (kg)", null=True, blank=True)
