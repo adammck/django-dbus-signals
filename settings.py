f68c55a3409d13d57e6c4006aa41f51bf64e12ce
adammck <adam.mckaig@gmail.com>
2010-03-14 20:57:32 -0400

    updated sample settings for clarity.

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ DATABASE_ENGINE = "sqlite3"
 DATABASE_NAME   = "db.sqlite3"
 
 
-DBUS_INTERFACE = "com.adammck.django"
-DBUS_PATH      = "/com/adammck/django"
+DBUS_INTERFACE = "com.adammck.dbus_example"
+DBUS_PATH      = "/com/adammck/dbus_example"
 
 
 INSTALLED_APPS = (
